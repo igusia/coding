a0a4f6f7698b1f843cc538bda3f9085355f4fc8d
igusia <iga.rygielska.source@gmail.com>
2024-09-22 23:05:36 -0700

    add queue reconstruct

---
+++ queuereconstruction/solution.py
@@ -0,0 +1,14 @@
+# T: O(nlogn)
+# S: O(n)
+class Solution:
+    def reconstruct(self, heights):
+        heights.sort(key=lambda x: (-x[0], x[1]))
+        result = []
+        for height in heights:
+            position = height[1]
+            result.insert(position, height)
+        return result
+
+
+heights = [[7, 0], [4, 4], [7, 1], [5, 0], [6, 1], [5, 2]]
+print(Solution().reconstruct(heights))
