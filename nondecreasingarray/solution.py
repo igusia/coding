6185a97966160cfb351cf297552377f5bb2bdd74
igusia <iga.rygielska.source@gmail.com>
2024-10-10 20:03:29 -0700

    cleanup algorithm

--- nondecreasingarray/solution.py
+++ nondecreasingarray/solution.py
@@ -1,19 +1,27 @@
+# T: O(n)
+# S: O(1)
 class Solution:
     def determine_non_decreasing(self, nums):
         if not nums or len(nums) < 3:
             return True
-        err = False
-        for i in range(1, len(nums) - 2):
-            if (nums[i - 1] < nums[i]) and (nums[i] > nums[i + 1]) and (nums[i + 1] < nums[i + 2]) and (nums[i - 1] > nums[i + 1]):
-                err = True
-            elif nums[i - 1] > nums[i] > nums[i + 1]:
-                err = True
-        i = len(nums) - 2
-        err = err or nums[i - 1] > nums[i] > nums[i + 1]
-        return not err
+
+        counter_modifications_needed = 0
+        for i in range(1, len(nums)):
+            if nums[i] < nums[i - 1]:
+                counter_modifications_needed += 1
+
+                if counter_modifications_needed > 1:
+                    return False
+
+                if i > 1 and nums[i - 2] > nums[i]:
+                    nums[i] = nums[i - 1]
+                else:
+                    nums[i - 1] = nums[i]
+
+        return True
 
 
-arr_1 = [1, 3, 2]
+arr_1 = [3, 2, 4, 1]
 arr_2 = [-1, 4, 2, 3]
 print(Solution().determine_non_decreasing(arr_1))
 print(Solution().determine_non_decreasing(arr_2))
